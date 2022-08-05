c52b8b49ba06937785cb8c4b07b45b60866e279e
LiuYinghao <liuyinghao4618@163.com>
2022-08-05 15:03:15 +0800

    接口等配置完成

---
+++ config/config.js
@@ -0,0 +1,4 @@
+const APIconfig={
+  baseURL:'https://admin.hxwendeng.com'
+}
+export {APIconfig}
---
+++ utils/http.js
@@ -0,0 +1,41 @@
+import {APIconfig} from '../config/config'
+import wxToPromise from './wx'
+
+class Http {
+  static async request({
+    url,
+    method = 'GET',
+    data = {}
+  }, options) {
+    const response = await wxToPromise('request', {
+      url: APIconfig.baseURL + url,
+      method,
+      data,
+      ...options
+    })
+
+    if (response.statusCode < 400) {
+      return response.data
+    }
+
+    if(response.statusCode==401){
+      // token过期,登录超时
+      return
+    }
+
+    Http._showError(response.data.code,response.data.msg)
+    return response
+  }
+
+  static _showError(code,msg){
+    let title=""
+    title=exceptionMessage[code]||msg||'发生未知错误'
+    wx.showToast({
+      title,
+      icon:'none',
+      duration:3000
+    })
+  }
+}
+
+export default Http
